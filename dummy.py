dc0b923f0b7fcad34cd7da849e1d9fe8363a3d22
Sambath <sambathvasu007@gmail.com>
2024-01-08 21:23:42 +0530

    Third commit

--- dummy.py
+++ dummy.py
@@ -3,17 +3,16 @@ import pyautogui
 import time
 
 # Recipient's WhatsApp number (including country code)
-phone_number = "+919486838162"
+phone_numbers = ["",""]  #"+9194*****3"
 
 # Message to be sent
 message = "!...."
 
 # Ensure WhatsApp Web is logged in and focused
-kit.sendwhatmsg_instantly(phone_number, message, wait_time=30)  # Allow ample time for loading
-time.sleep(20)  # Wait for message box to appear
-
-
-pyautogui.press('enter')
+for phone_number in phone_numbers:
+    kit.sendwhatmsg_instantly(phone_number, message, wait_time=30)  # Allow ample time for loading
+    time.sleep(20)  # Wait for message box to appear
+    pyautogui.press('enter')
 
 
 print("Message sent successfully!")
