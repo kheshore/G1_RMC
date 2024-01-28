6201205c3ec5287673b6cae41bd618ddbc6c6af4
Sambath <sambathvasu007@gmail.com>
2024-01-28 13:04:46 +0530

    Django Initiation

---
+++ G1_RMC_Django/G1_RMC_App/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class G1RmcAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'G1_RMC_App'
--- dummy.py
+++ dummy.py
@@ -1,19 +1,19 @@
-import pywhatkit as kit
-import pyautogui
-import time
+# import pywhatkit as kit
+# import pyautogui
+# import time
 
-# Recipient's WhatsApp number (including country code)
-phone_numbers = ["",""]  #"+9194*****3"
+# # Recipient's WhatsApp number (including country code)
+# phone_numbers = ["",""]  #"+9194*****3"
 
-# Message to be sent to the phone_numbers through whatsapp
-message = "!...."
+# # Message to be sent to the phone_numbers through whatsapp
+# message = "!...."
 
-# Ensure WhatsApp Web is logged in and focused
-for phone_number in phone_numbers:
-    kit.sendwhatmsg_instantly(phone_number, message, wait_time=30)  # Allow ample time for loading
-    time.sleep(20)  # Wait for message box to appear
-    pyautogui.press('enter')
+# # Ensure WhatsApp Web is logged in and focused
+# for phone_number in phone_numbers:
+#     kit.sendwhatmsg_instantly(phone_number, message, wait_time=30)  # Allow ample time for loading
+#     time.sleep(20)  # Wait for message box to appear
+#     pyautogui.press('enter')
 
 
 
-print("Message sent successfully!")
+# print("Message sent successfully!")
