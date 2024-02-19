563de426e07255a595e8cb716b42925066728e76
Sambath <sambathvasu007@gmail.com>
2024-02-19 21:06:35 +0530

    Register page and login page added

--- G1_RMC_Django/authentication/urls.py
+++ G1_RMC_Django/authentication/urls.py
@@ -1,6 +1,9 @@
-from . import views
+from .views import registerView, loginView, emailValidation
 from django.urls import path
+from django.views.decorators.csrf import csrf_exempt
 
 urlpatterns = [
-    path('login/',views.login,name="login")
+    path('login',loginView.as_view(),name="login"),
+    path('register',registerView.as_view(),name="login"),
+    path('validate_email',csrf_exempt(emailValidation.as_view()),name="validate_email"),
 ]
--- G1_RMC_Django/authentication/views.py
+++ G1_RMC_Django/authentication/views.py
@@ -1,5 +1,28 @@
+import json
+from django.views import View
 from django.shortcuts import render
+from django.http import JsonResponse
+from validate_email import validate_email
+from django.contrib.auth.models import User
 
 # Create your views here.
-def login(request):
-    return render(request,'authentication/login.html')
+class loginView(View):
+    def get(self,request):
+        return render(request,'authentication/login.html')
+
+class registerView(View):
+    def get(self,request):
+        return render(request,'authentication/register.html')
+    
+class emailValidation(View):
+    import json
+    def post(self,request):
+        data = json.loads(request.body)
+        email = data['email']
+        if not validate_email(email):
+            return JsonResponse({'email_error' : 'Email is invalid'}, status = 400) 
+        if User.objects.filter(email=email).exists():
+            return JsonResponse({'email_invalid' : 'Sorry, Email already in Use, Choose another'}, status = 400)
+        return JsonResponse({'email_valid' : True})
+
+        
